d99e5ff5a22cc6bcd2f31960c8025b4ec7a3f51b
JMGbiocomp <joshuaglowalla@gmail.com>
2026-02-25 23:49:57 -0800

    feature: RNAseq analysis regression modeling function
    Code updates to function as well as added functionality to recognize and model for data with feature (response) variable with 3 or more categories, both nominal and ordinal substypes.  The 'nnet' and 'MASS' packages were added to include modeling functions thjat can handle responses variables of these types.

--- function_regressionModel.R
+++ function_regressionModel.R
@@ -1,15 +1,20 @@
 ### === Linear regression modeling === ###
 # A function that automates the generation of a linear regression model by evaluating the response variable to determine the most appropriate error distribution and link function
-# Functionality includes user override of embedded algorithm to determine the 'family' argument of the glm() function as well as modifying the response variable to becoem binary as either a measure of treatment success or 2-level categorical
+# Functionality includes user override of embedded algorithm to determine the 'family' argument of the glm() function, modifying the response variable to become binary as either a measure of treatment success or 2-level categorical, and modeling of categorical response varaibles with 3 or more levels (nomial and ordinal)
   # model_data = independent variable for modeling with samples as columns and species as rows
   # feature_labels = feature variable to be used as response variable for modeling
   # model_formula = formula to build the model; default set to 'y ~ .' to include all species in the model from the data set
   # glm_family = determines the error distribution and link function for the glm() function ('family'); leave as default (NA) to allow the embedded algorithm determine the most appropriate family argument
-  # binarize = boulean value to determine if the feature variables will be modified to become binary
-  # b_type = determines the type of binary variable; must choose either 'categories' or 'success' 
-  # binary_cutoff =. single numeric value vector to use as the cut off value when modifying the feature variable to become binary
-library(stats)
-regressionModel = function (model_data = NA, feature_labels = NA, model_formula = y ~ ., glm_family = NA, binarize = FALSE, b_type = c("categories", "success"), binary_cutoff = NA) {
+  # binarize = logical value to determine if the feature variables will be modified to become binary
+  # b_type = determines the type of binary variable; must choose either 'categories' or 'success' in which the response variable is converted into a 2-level categorical variable (high vs low) or representative of treatment success with 1 designating a successful response, respectively
+  # binary_cutoff = single numeric value vector to use as the cut off value when modifying the feature variable to become binary
+  # response_ordered = logical value to indicate that the categorical feature variable (response) is unordered (nominal) or ordered (ordinal), which helps defines the regression model function and factorization of the response variable
+  # feature_order = character vector that defines the order of factors for the feature (response) variable 
+  # hessian = logical for whether the Hessian (the observed/expected information matrix) should be returned; default set to TRUE
+  # model_frame = a logical value indicating whether model frame should be included as a component of the returned value; default set to TRUE
+
+library(stats); library(nnet); library(MASS)
+regressionModel = function (model_data = NA, feature_labels = NA, model_formula = y ~ ., glm_family = NA, binarize = FALSE, b_type = c("categories", "success"), binary_cutoff = NA, response_ordered = FALSE, feature_order = NA, hessian = TRUE, model_frame = TRUE) {
   # errors and flags
   if (is.na(model_data)) {stop("requires a data matrix or equivalaent to model")}
   if (is.na(feature_labels)) {stop("requires a data feature as a respoinse variable to model against")}
@@ -80,14 +85,34 @@ regressionModel = function (model_data = NA, feature_labels = NA, model_formula 
       glm_family = Gamma() # recognized as continuous variable feature that is always positive and skewed
     } else if (continuous == TRUE && string == FALSE) {
       glm_family = gaussian() # recognized as a true continuous variable that follows a normal distribution
-    } else if (binary == TRUE && string == TRUE) {
+    } else if (binary == TRUE && string == TRUE && length(unique(feature_labels))<3) {
       glm_family = binomial # recognized binomial data as a 2-level category variable
-      feature_labels = as.factor(feature_labels)
+      if ("low" %in% feature_labels) {
+        feature_labels = as.factor(feature_labels, levels = c("low", "high"))
+      } else {
+        feature_labels = as.factor(feature_labels)
+      }
+    } else if (binary == FALSE && string == TRUE && length(unique(feature_labels))<2) { # recognized feature data as a 3 or more level categorical variable 
+      glm_family = "multi-level"
     } else {
-      stop("Error distributiona and link function cannot be determined by embedded algorithm. Mannually adjust feature variable to desired structure or set 'glm_family' argument.")
+      stop("Error distribution and link function cannot be determined by embedded algorithm. Mannually adjust feature variable to desired structure and type or set 'glm_family' argument.")
     }
   }
-  y = feature_labels # sets the feature varaible as the dependent response variable 
-  model = glm(formula = model_formula, family = glm_family, data = t(model_data)) # generate the generalized linear model to the determined specs
+  if (glm_family == "multi-level") {
+    # flow control to build a model with the feature (response) variable as ordinal or multinomical 
+    if (response_ordered == TRUE) {
+      feature_labels = ordered(feature_labels, levels = feature_order) # ordered factorization
+      y = feature_labels
+      model = polr(formula = model_formula, data = t(model_data), model = model_frame, Hess = hessian) # default is set to include Hessian matrix and model frame are included in the model object
+    } else {
+      feature_labels = as.factor(feature_labels) # nomial factorization
+      y = feature_labels
+      model = multinom(formula = model_formula, data = model_data, model = model_frame, Hess = hessian) # default is set to include Hessian matrix and model frame are included in the model object
+    }
+  } else {
+    y = feature_labels # sets the feature variable as the dependent response variable 
+    model = glm(formula = model_formula, family = glm_family, data = t(model_data), model = model_frame) # generate the generalized linear model to the determined specs
+  }
+  
   return(model)
 }
